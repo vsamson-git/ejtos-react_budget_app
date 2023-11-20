6d3f13355138f6a3d79be04d87f7372368a083c4
vsamson-git <vsamson.my@gmail.com>
2023-11-20 15:31:40 -0500

    small update

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,19 +2,23 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 export default () => {
-    const { dispatch } = useContext(AppContext);
-    const handleChange = (event) => {
+    const { dispatch, currency } = useContext(AppContext);
+    const handleClick = (event) => {
+        console.log(event.target['value']);
         dispatch({type: 'CHG_CURRENCY', payload: event.target.value});
     }
+    const currency_list = {"$": "Dollar", "£": "Pound", "€": "Euro", "₹": "Ruppee"}
 
     return (
         <div id="currency-change">
-            <select name="currency" id="currency-select" onChange={handleChange}>
-                <option value="$">$ Dollar</option>
-                <option value="£" selected>£ Pound</option>
-                <option value="€">€ Euro</option>
-                <option value="₹">₹ Ruppee</option>
-            </select>
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Currency ({currency} {currency_list[currency]})
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    {Object.keys(currency_list).map(key => (<a className="dropdown-item" href="#" onClick={handleClick} value={key} key={key}>{key} {currency_list[key]}</a>))}
+                </div>
+                </div>
         </div>
     )
 }
